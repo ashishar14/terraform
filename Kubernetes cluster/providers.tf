5d33bc7113f8fdf4d33bd7f1d64339b467dd7666
Ashish <ashishar14@outlook.com>
2026-06-18 12:08:48 +0530

    Add Kubernetes cluster Terraform project

---
+++ Kubernetes cluster/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = var.aws_profile
+  region  = var.region
+
+  default_tags {
+    tags = {
+      ManagedBy = "Terraform"
+      Project   = "EKS-Demo"
+    }
+  }
+}
